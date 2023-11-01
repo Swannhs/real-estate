5730db3a13847308258e82cacd361a019bce8e2a
swann <swann@fortunatis.ch>
2023-11-02 01:03:58 +0600

    Initialize static service

---
+++ static-data-service/src/test/java/com/fortunatis/staticdataservice/StaticDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortunatis.staticdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
