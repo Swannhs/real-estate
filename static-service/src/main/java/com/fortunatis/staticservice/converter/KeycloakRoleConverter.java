2328f90563c77276d2cee44c33eab7bef122415f
Swannhs <princeapurbask@gmail.com>
2024-01-26 13:32:06 -0500

    Implemented nginx api gateway

--- static-service/src/main/java/com/fortunatis/staticservice/converter/KeycloakRoleConverter.java
+++ static-service/src/main/java/com/fortunatis/staticservice/converter/KeycloakRoleConverter.java
@@ -15,11 +15,35 @@ import java.util.stream.Stream;
 
 @Component
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
-
     @Override
     public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
         Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
 
-        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+        List<String> realmRoles = extractRoles(realmAccess);
+        List<String> resourceRoles = extractResourceRoles(resourceAccess);
+
+        List<String> allRoles = Stream.concat(realmRoles.stream(), resourceRoles.stream()).toList();
+
+        return allRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> extractRoles(Map<String, Object> realmAccess) {
+        if (realmAccess != null && realmAccess.containsKey("roles")) {
+            return (List<String>) realmAccess.get("roles");
+        }
+        return Collections.emptyList();
+    }
+
+    private List<String> extractResourceRoles(Map<String, Object> resourceAccess) {
+        if (resourceAccess != null && resourceAccess.containsKey("account") && resourceAccess.get("account") instanceof Map) {
+            Map<String, Object> accountRoles = (Map<String, Object>) resourceAccess.get("account");
+            if (accountRoles.containsKey("roles")) {
+                return (List<String>) accountRoles.get("roles");
+            }
+        }
+        return Collections.emptyList();
     }
 }
